1a220940fa0b57dcd72ed0d2037d1b94332d13f7
kskyasser <kossokoyasser@gmail.com>
2026-06-21 23:53:57 +0200

    Intégration HTML/CSS des 5 pages (accueil, catalogue, fiche produit, panier, admin)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/catalogue', name: 'app_catalogue')]
+    public function catalogue(): Response
+    {
+        return $this->render('home/catalogue.html.twig');
+    }
+
+    #[Route('/produit', name: 'app_produit')]
+    public function produit(): Response
+    {
+        return $this->render('home/produit.html.twig');
+    }
+
+    #[Route('/panier', name: 'app_panier')]
+    public function panier(): Response
+    {
+        return $this->render('home/panier.html.twig');
+    }
+
+    #[Route('/connexion', name: 'app_connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('home/connexion.html.twig');
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function admin(): Response
+    {
+        return $this->render('home/admin.html.twig');
+    }
+}
